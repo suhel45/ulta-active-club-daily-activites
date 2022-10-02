71787f4cc1d88cdbbf383405f14f28fc3fe50a22
suhel45 <suhel.ict45@gmail.com>
2022-10-02 23:49:43 +0600

    break btn added

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
 import logo from '../../logo.svg'
 const List = (props) => {
     let TotalTime = 0;
     for(const time of props.list){
         TotalTime = TotalTime + parseFloat(time.time);
+    }
+    const addBreakTime = (breakTime) =>{
+        console.log(breakTime)
     }
     return (
         <div>
@@ -19,7 +21,7 @@ const List = (props) => {
  <div>
      <h3>Add Break</h3>
      <div className='btn'>
-         <button className='rounded-circle bg-white border-0 py-1'>10s</button>
+         <button onClick={() =>addBreakTime(10)} className='rounded-circle bg-white border-0 py-1'>10s</button>
          <button className='rounded-circle bg-white border-0 py-1'>20s</button>
          <button className='rounded-circle bg-white border-0 py-1'>30s</button>
          <button className='rounded-circle bg-white border-0 py-1'>40s</button>
